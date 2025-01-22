f0771b5c2f4c5cd65499b482d4d6ef66a2d512dc
Seminish2001 <samuel_prenga@yahoo.com>
2025-01-22 23:19:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     
-@app.route('/properties-page')
+@app.route('/properties')
 def serve_properties_page():
     properties = Property.query.all()
     return render_template('properties.html', properties=properties)
