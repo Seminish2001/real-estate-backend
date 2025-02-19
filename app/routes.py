460f151099ebf44c35a44daba58ee44b2ff47771
Seminish2001 <samuel_prenga@yahoo.com>
2025-02-20 00:12:47 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,45 +1,30 @@
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_jwt_cookies
-from app.models import db, User, bcrypt
+from flask import Blueprint, jsonify, request, render_template
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import db, User
 
-auth_routes = Blueprint("auth_routes", __name__)
+routes = Blueprint("routes", __name__)
 
-@auth_routes.route("/register", methods=["POST"])
-def register():
-    try:
-        data = request.json
-        if User.query.filter_by(email=data["email"]).first():
-            return jsonify({"message": "Email already registered!"}), 409
+@routes.route("/dashboard")
+@jwt_required()
+def serve_dashboard():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user:
+        return render_template("dashboard.html", user=user)
+    return jsonify({"message": "User not found"}), 404
 
-        new_user = User(
-            name=data["name"],
-            email=data["email"],
-            user_type=data["user_type"]
-        )
-        new_user.set_password(data["password"])
-        db.session.add(new_user)
-        db.session.commit()
+@routes.route("/properties")
+def serve_properties():
+    return render_template("properties.html")
 
-        return jsonify({"message": "User registered successfully!"}), 201
-    except Exception as e:
-        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+@routes.route("/alerts")
+def serve_alerts():
+    return render_template("alerts.html")
 
-@auth_routes.route("/login", methods=["POST"])
-def login():
-    data = request.json
-    user = User.query.filter_by(email=data["email"]).first()
-    if user and user.check_password(data["password"]):
-        access_token = create_access_token(identity=user.id)
-        refresh_token = create_refresh_token(identity=user.id)
-        
-        response = jsonify({"message": "Login successful!"})
-        set_access_cookies(response, access_token)
-        set_refresh_cookies(response, refresh_token)
-        return response, 200
-    return jsonify({"message": "Invalid credentials!"}), 401
+@routes.route("/terms")
+def terms():
+    return render_template("terms.html")
 
-@auth_routes.route("/logout", methods=["POST"])
-def logout():
-    response = jsonify({"message": "Successfully logged out!"})
-    unset_jwt_cookies(response)
-    return response
+@routes.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
