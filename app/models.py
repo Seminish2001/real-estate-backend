4fb50728a36474e09e7d175b6d24f448daa05d37
Seminish2001 <samuel_prenga@yahoo.com>
2025-02-20 00:07:50 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    user_type = db.Column(db.String(50), nullable=False)  # "Private Owner" or "Real Estate Agency"
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
