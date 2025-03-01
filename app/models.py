cdbcb01356ae37657ab78c66e7f3b9780d6447c9
Seminish2001 <samuel_prenga@yahoo.com>
2025-03-02 00:49:26 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    user_type = db.Column(db.String(50), nullable=False)  # "Private Owner" or "Real Estate Agency"
+    user_type = db.Column(db.String(50), nullable=False)  # "Private Owner", "Real Estate Agency", "Buyer/Renter"
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
