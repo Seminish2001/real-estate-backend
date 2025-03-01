0b438e17f920c4364f473d73e499aae8ccf43684
Seminish2001 <samuel_prenga@yahoo.com>
2025-03-02 00:51:45 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,32 @@ from app.models import db, User
 
 routes = Blueprint("routes", __name__)
 
-@routes.route("/dashboard")
+@routes.route("/dashboard/agency")
 @jwt_required()
-def serve_dashboard():
+def serve_dashboard_agency():
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
-    if user:
+    if user and user.user_type == "Real Estate Agency":
         return render_template("dashboard.html", user=user)
-    return jsonify({"message": "User not found"}), 404
+    return jsonify({"message": "Unauthorized or user not found"}), 403
+
+@routes.route("/dashboard/private")
+@jwt_required()
+def serve_dashboard_private():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user and user.user_type == "Private Owner":
+        return render_template("dashboard-private.html", user=user)
+    return jsonify({"message": "Unauthorized or user not found"}), 403
+
+@routes.route("/dashboard/buyer-renter")
+@jwt_required()
+def serve_dashboard_buyer_renter():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user and user.user_type == "Buyer/Renter":
+        return render_template("dashboard-buyer-renter.html", user=user)
+    return jsonify({"message": "Unauthorized or user not found"}), 403
 
 @routes.route("/properties")
 def serve_properties():
