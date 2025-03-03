e1be6b717f53c5d9f8ce4a777b44d3884a64680c
Seminish2001 <samuel_prenga@yahoo.com>
2025-03-03 22:37:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import os
 from app import create_app
-from app.models import db
 
-# Initialize Flask App
 app = create_app()
 
-# Ensure Database Tables Exist
-with app.app_context():
-    db.create_all()
-
-# Run the Application
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
