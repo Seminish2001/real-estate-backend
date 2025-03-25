746d2d64036f052ddd7e304a14f880b8eef078c3
Seminish2001 <samuel_prenga@yahoo.com>
2025-03-25 12:28:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
 
 app = create_app()
+
+# Automate migrations on startup
+with app.app_context():
+    Migrate(app, db)  # Initialize migrations
+    from flask_migrate import upgrade
+    upgrade()  # Apply migrations to create tables
 
 if __name__ == '__main__':
     app.run(debug=True)
