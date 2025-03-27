c8f5ce14f4bcc9aef7aa2dfd681602ca45215b49
Seminish2001 <samuel_prenga@yahoo.com>
2025-03-27 19:28:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from app import create_app
-import os
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
     # For local development only
+    import os
     port = int(os.getenv("PORT", 5000))  # Use PORT env var if set, else default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
