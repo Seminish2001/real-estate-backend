97a27d0edf900a24a5eec9d183f3e933690b831d
Seminish2001 <samuel_prenga@yahoo.com>
2025-06-05 11:55:14 +0200

    Revert "Modify homepage UI"

--- app.py
+++ app.py
@@ -379,10 +379,6 @@ def terms_page():
 def privacy_page():
     return render_template("privacy.html")
 
-@app.route("/newsletter-preferences")
-def newsletter_preferences_page():
-    return render_template("newsletter_preferences.html")
-
 @app.route("/agents")
 def agents_page():
     return render_template("agents.html")
